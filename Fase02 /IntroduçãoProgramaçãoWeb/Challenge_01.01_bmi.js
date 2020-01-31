8155f2f09aa19942d1bd2fa7cac8a421c523ea21
Kayo Renato <kayorenatocontato@gmail.com>
2020-01-31 00:44:51 -0300

    Adjusted directory name

--- Fase02 /IntroduçãoProgramaçãoWeb/Challenge_01.01_bmi.js
+++
@@ -1,7 +0,0 @@
-const name = 'kayo';
-const weight = 58;
-const height = 1.71;
-
-IMC = weight / (height * height);
-
-IMC >= 30 ? console.log(`${name} você está acima do peso.`) :  console.log(`${name} você não está acima do peso.`)
